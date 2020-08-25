500abac191ac0965b497a0a814e737e4dd3d0f3f
jandersen770 <jandersen770@gmail.com>
2020-08-25 13:23:47 -0400

    Update

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -17,12 +17,11 @@ class Api::CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = @commentable.comments.create(comment_params)
-    respond_to do |format|
-      if @comment.save
-        render :show
-      else
-        render json: @comment.errors.full_messages, status: 422
-      end
+
+    if @comment.save
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 422
     end
   end
 
