acec3736234a91f70fb427e58e895c829fb2bd78
jandersen770 <jandersen770@gmail.com>
2020-10-29 22:32:50 -0400

    update

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,5 @@ class Comment < ApplicationRecord
     belongs_to :user,
         foreign_key: :user_id,
         class_name: :User
+    # belongs_to :comment, -> { where(commentable_type: 'Comment') }, foreign_key: :commentable_id
 end
